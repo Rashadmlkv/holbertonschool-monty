6f052227a70766ae67713bc47a9911babfb71311
Rashadmlkv <resadmlkv@gmail.com>
2024-01-04 15:49:56 -0800

    added description

--- freeStack.c
+++ freeStack.c
@@ -1,14 +1,18 @@
 #include "monty.h"
 
+/**
+ * freeStack - frees nodes
+ * @stack: stack
+ */
 void freeStack(stack_t *stack)
 {
 	stack_t *temp = stack;
 
 	for (; temp != NULL; temp = temp->next)
 	{
-		if(temp->prev != NULL)
+		if (temp->prev != NULL)
 			free(temp->prev);
-		else if(temp->next == NULL)
+		else if (temp->next == NULL)
 			free(temp);
 	}
 }
