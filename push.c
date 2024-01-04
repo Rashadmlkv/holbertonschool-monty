5009ad852a5fd73416757667833052ad5fd37fff
Rashad <58619029+Rashadmlkv@users.noreply.github.com>
2024-01-05 03:34:13 +0400

    Update push.c

--- push.c
+++ push.c
@@ -1,6 +1,4 @@
 #include "monty.h"
-
-
 /**
  * push - pushes data into stack
  * @stack: top of stack
@@ -9,25 +7,23 @@
 void push(stack_t **stack, unsigned int line_number)
 {
 	stack_t *new_node = malloc(sizeof(stack_t));
-
 	if (!new_node)
 	{
 		fprintf(stderr, "Error: malloc failed\n");
-	        freeStack(*stack);
+		free(new_node);
+		free(*stack);
 		exit(EXIT_FAILURE); }
 
-	if (isNumber(glob.num) == 0 || strcmp(glob.num, "") == 0)
+	if (isNumber(glob.num) == 0)
 	{
 		fprintf(stderr, "L%d: usage: push integer\n", line_number);
-		freeStack(*stack);
+		free(new_node);
+		free(*stack);
 		exit(EXIT_FAILURE); }
-
-        new_node->n = atoi(glob.num);
+	new_node->n = atoi(glob.num);
 	new_node->prev = NULL;
 	new_node->next = *stack;
-
 	if (*stack)
 		(*stack)->prev = new_node;
-
 	*stack = new_node;
 }
