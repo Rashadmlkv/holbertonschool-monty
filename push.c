b4133107f11b1e9b525a2c3e1dbfcedc12adf34f
Rashad <58619029+Rashadmlkv@users.noreply.github.com>
2024-01-05 03:35:28 +0400

    Update push.c

--- push.c
+++ push.c
@@ -10,19 +10,19 @@ void push(stack_t **stack, unsigned int line_number)
 	if (!new_node)
 	{
 		fprintf(stderr, "Error: malloc failed\n");
-		free(new_node);
-		free(*stack);
+	        freeStack(*stack);
 		exit(EXIT_FAILURE); }
 
-	if (isNumber(glob.num) == 0)
+	if (isNumber(glob.num) == 0 || strcmp(glob.num, "") == 0)
 	{
 		fprintf(stderr, "L%d: usage: push integer\n", line_number);
-		free(new_node);
-		free(*stack);
+		freeStack(*stack);
 		exit(EXIT_FAILURE); }
-	new_node->n = atoi(glob.num);
+
+        new_node->n = atoi(glob.num);
 	new_node->prev = NULL;
 	new_node->next = *stack;
+
 	if (*stack)
 		(*stack)->prev = new_node;
 	*stack = new_node;
