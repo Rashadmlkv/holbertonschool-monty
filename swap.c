99b34993c1d39d634911cae5707cabc383f29871
Rashadmlkv <resadmlkv@gmail.com>
2024-01-07 23:09:30 -0800

    second commit

--- swap.c
+++ swap.c
@@ -9,7 +9,7 @@ void swap(stack_t **stack, unsigned int line_number)
 {
 	stack_t *tmp = NULL;
 
-	if (!(*stack)->next)
+	if (!(*stack)->next || !(*swap))
 	{
 		fprintf(stderr, "L%d: can't swap, stack too short\n", line_number);
 		freeStack();
