208f44c06d6240c6c6afa60b57a1266a56c1af67
Rashadmlkv <resadmlkv@gmail.com>
2024-01-08 10:34:04 -0800

    typo change

--- isNumber.c
+++ isNumber.c
@@ -2,7 +2,7 @@
 /**
  * isNumber - check string is full of numbers or not
  * @number: string
- * Return: if string is not number return 0
+ * Return: if string is not number return -1
  */
 int isNumber(char *number)
 {
@@ -13,6 +13,6 @@ int isNumber(char *number)
 		if(i == 0 && number[0] == '-')
 			++i;
 		if ((isdigit(number[i])) == 0)
-			return (0); }
-	return (-1);
+			return (-1); }
+	return (0);
 }
