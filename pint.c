38caf5493fb17bd09483f5c0400e39d7f158eae4
Rashadmlkv <resadmlkv@gmail.com>
2024-01-07 22:24:03 -0800

    first commit

---
+++ pint.c
@@ -0,0 +1,19 @@
+#include "monty.h"
+
+/**
+ * pint - print head of stack
+ * @stack: stack
+ * @line_number: line number where command occured
+ *
+ */
+void pint(stack_t **stack, unsigned int line_number)
+{
+	if (!stack)
+	{
+		fprintf(stderr, "L%d: can't pint, stack empty\n", line_number);
+		freeStack();
+		fclose(glob.fd);
+		exit(EXIT_FAILURE);
+	}
+	printf("%d\n", (*stack)->n);
+}
