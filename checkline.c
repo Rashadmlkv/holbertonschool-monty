ca1987e5273a7fc1444adaf60c853a2c2ef71ef9
Rashadmlkv <resadmlkv@gmail.com>
2024-01-08 10:50:07 -0800

    just checking

--- checkline.c
+++ checkline.c
@@ -12,11 +12,11 @@ int checkline(FILE *fd)
 
 	for (i = 1; (getline(&glob.buffer, &max, fd)) > 0; i++)
 	{
-		token = strtok(glob.buffer, " \t$\n");
+		token = strtok(glob.buffer, " \t\n");
 		if (token)
 		{
 			glob.f = checkop(token, i);
-			token = strtok(NULL, " \t$\n");
+			token = strtok(NULL, " \t\n");
 			if (token)
 				glob.num = token;
 			glob.f(&glob.top, i);
