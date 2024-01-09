ab2f5a7df49303ba3e1ed16f143c71ebd5987dc0
lunarinaa <aydan.akbarova@gmail.com>
2024-01-09 02:16:06 -0800

    header file updated

--- monty.h
+++ monty.h
@@ -73,4 +73,5 @@ void swap(stack_t **stack, unsigned int line_number);
 void add(stack_t **stack, unsigned int line_number);
 void nop(stack_t **stack, unsigned int line_number);
 void sub(stack_t **stack, unsigned int line_number);
+void division(stack_t **stack, unsigned int line_number);
 #endif
