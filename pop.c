a28d1053d41fea4fd434ec1dad1ef8b0c062a907
Rashadmlkv <resadmlkv@gmail.com>
2024-01-08 22:44:42 -0800

    Cmon

--- pop.c
+++ pop.c
@@ -7,11 +7,11 @@
  */
 void pop(stack_t **stack, unsigned int line_number)
 {
-	stack_t *tmp;
+	stack_t *tmp = NULL;
 
 	if (!(*stack))
 	{
-		fprintf(stderr, "L%d: can't pop an empty stack\n", line_number);
+		fprintf(stderr, "L%u: can't pop an empty stack\n", line_number);
 		fclose(glob.fd);
 		exit(EXIT_FAILURE);
 	}
