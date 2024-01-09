16256406c2d7ef531c20e5a0ed5594719826408f
Rashadmlkv <resadmlkv@gmail.com>
2024-01-08 22:31:10 -0800

    changed output stream

--- pint.c
+++ pint.c
@@ -10,9 +10,9 @@ void pint(stack_t **stack, unsigned int line_number)
 {
 	if (!stack)
 	{
-		fprintf(stderr, "L%d: can't pint, stack empty\n", line_number);
+		fprintf(stderr, "L%u: can't pint, stack empty\n", line_number);
 		fclose(glob.fd);
 		exit(EXIT_FAILURE);
 	}
-	printf("%d\n", (*stack)->n);
+	fprintf(stdout,"%d\n", (*stack)->n);
 }
