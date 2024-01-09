91a7f4a997ac83056c754a92c39461786e4f5dd9
Rashadmlkv <resadmlkv@gmail.com>
2024-01-08 23:02:52 -0800

    Bored

--- pop.c
+++ pop.c
@@ -12,7 +12,7 @@ void pop(stack_t **stack, unsigned int line_number)
 	if (!stack || !(*stack))
 	{
 		fprintf(stderr,	"L%u: can't pop an empty stack\n", line_number);
-		freeStack();
+		fclose(glob.fd);
 		exit(EXIT_FAILURE);
 	}
 
