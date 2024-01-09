4fa6238de95f3bd17acd5eb5fca017188aa7d1f4
lunarinaa <aydan.akbarova@gmail.com>
2024-01-09 01:50:30 -0800

    error message fixed

--- sub.c
+++ sub.c
@@ -15,7 +15,7 @@ void sub(stack_t **stack, unsigned int line_number)
 	if (!stack || !*stack || !(*stack)->next)
 	{
 		freeStack();
-		dprintf(2, "L%u: can't add, stack too short\n", line_number);
+		dprintf(2, "L%u: can't sub, stack too short\n", line_number);
 		exit(EXIT_FAILURE);
 	}
 
