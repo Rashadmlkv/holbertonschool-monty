c4ed662affea2b10d73230472d1a36584db643c0
Rashadmlkv <resadmlkv@gmail.com>
2024-01-08 22:59:22 -0800

    MYAU

--- pop.c
+++ pop.c
@@ -12,7 +12,7 @@ void pop(stack_t **stack, unsigned int line_number)
 	if (!stack || !(*stack))
 	{
 		fprintf(stderr,	"L%u: can't pop an empty stack\n", line_number);
-		free_glob();
+		freeStack();
 		exit(EXIT_FAILURE);
 	}
 
