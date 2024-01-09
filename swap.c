a305f5f8f89961ee6cd48a793a7eebb4ff2e5e7f
Rashadmlkv <resadmlkv@gmail.com>
2024-01-08 22:25:51 -0800

    first

---
+++ swap.c
@@ -0,0 +1,30 @@
+#include "monty.h"
+/**
+ * swap - swap elemts in stack
+ * @stack: head of stack
+ * @line_number: command occured in file
+ */
+void swap(stack_t **stack, unsigned int line_number)
+{
+	stack_t *tmp = NULL;
+
+	if (!(*stack)->next || !(*stack))
+	{
+		fprintf(stderr, "L%d: can't swap, stack too short\n", line_number);
+		freeStack();
+		fclose(glob.fd);
+		exit(EXIT_FAILURE);
+	}
+	tmp = malloc(sizeof(stack_t));
+	if(!tmp)
+	{
+		fprintf(stderr, "Error: malloc failed\n");
+		freeStack();
+		fclose(glob.fd);
+		exit(EXIT_FAILURE);
+	}
+	tmp->n = (*stack)->n;
+	(*stack)->n = (*stack)->next->n;
+	(*stack)->next->n = tmp->n;
+	free(tmp);
+}
