d3cbf5aec38903cd3502d803902cc38ad803d93f
lunarinaa <aydan.akbarova@gmail.com>
2024-01-09 02:24:13 -0800

    function declaration provided

--- monty.h
+++ monty.h
@@ -74,4 +74,5 @@ void add(stack_t **stack, unsigned int line_number);
 void nop(stack_t **stack, unsigned int line_number);
 void sub(stack_t **stack, unsigned int line_number);
 void division(stack_t **stack, unsigned int line_number);
+void mul(stack_t **stack, unsigned int line_number);
 #endif
